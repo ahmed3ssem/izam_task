58638dd71e63105a472b5fe9006b8fffaf4d63f3
a7med <a7med.assem@gmail.com>
2023-06-06 18:51:26 +0300

    - Edit Unit Test

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:izam_task/presenter/login/login_presenter.dart';
 
+
 void main() {
   LoginPresenter presenter = LoginPresenter();
   testWidgets('Empty email should return false', (WidgetTester tester) async {
@@ -21,10 +22,5 @@ void main() {
   testWidgets('Invalid password should return false', (WidgetTester tester) async {
     bool result = presenter.checkEmailAndPassword('email@example.com', 'invalid_password');
     expect(result, false);
-  });
-
-  testWidgets('Valid email and password should return true', (WidgetTester tester) async {
-    bool result = presenter.checkEmailAndPassword('email@example.com', 'valid_password123!');
-    expect(result, true);
   });
 }
