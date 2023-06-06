d64eac0e9f347eafeb4135043702d125af2f72fb
a7med <a7med.assem@gmail.com>
2023-06-06 14:55:28 +0300

    - Refactor Code

--- lib/view/login/login_view.dart
+++ lib/view/login/login_view.dart
@@ -16,12 +16,10 @@ class LoginView extends StatefulWidget {
   State<LoginView> createState() => _LoginViewState();
 }
 
-class _LoginViewState extends State<LoginView> implements LoginViewComponent{
-
+class _LoginViewState extends State<LoginView> implements LoginViewComponent {
   late LoginPresenterComponent component;
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-
 
   @override
   void initState() {
@@ -37,45 +35,94 @@ class _LoginViewState extends State<LoginView> implements LoginViewComponent{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/images/logo.jpeg' , width: ScreenUtil().setWidth(141),height: ScreenUtil().setHeight(37),),
-            SizedBox(height: ScreenUtil().setHeight(20),),
+            Image.asset(
+              'assets/images/logo.jpeg',
+              width: ScreenUtil().setWidth(141),
+              height: ScreenUtil().setHeight(37),
+            ),
+            SizedBox(height: ScreenUtil().setHeight(20)),
             SingleChildScrollView(
               child: SizedBox(
                 width: ScreenUtil().setWidth(350),
                 height: ScreenUtil().setHeight(300),
-                child:  Card(
+                child: Card(
                   elevation: 8,
-                  child:Container(
+                  child: Container(
                     margin: EdgeInsets.only(top: ScreenUtil().setHeight(30)),
-                    child:  Column(
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Text('loginPhase'.tr , style: TextStyle(color: AppColors.fontColor , fontSize: 28.sp , fontWeight: FontWeight.bold),),
-                        SizedBox(height: ScreenUtil().setHeight(20),),
-                        TextFieldWidget(height: ScreenUtil().setHeight(50), width: ScreenUtil().setWidth(290), controller: emailController, prefixIcon: const SizedBox(), hintText: 'email'.tr, isPasswordTextField: false, keyboardType: TextInputType.emailAddress) ,
-                        TextFieldWidget(height: ScreenUtil().setHeight(50), width: ScreenUtil().setWidth(290), controller: passwordController, prefixIcon: const SizedBox(), hintText: 'password'.tr, isPasswordTextField: true, keyboardType: TextInputType.text),
+                        Text(
+                          'loginPhase'.tr,
+                          style: TextStyle(
+                            color: AppColors.fontColor,
+                            fontSize: 28.sp,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: ScreenUtil().setHeight(20)),
+                        TextFieldWidget(
+                          height: ScreenUtil().setHeight(50),
+                          width: ScreenUtil().setWidth(290),
+                          controller: emailController,
+                          prefixIcon: const SizedBox(),
+                          hintText: 'email'.tr,
+                          isPasswordTextField: false,
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        TextFieldWidget(
+                          height: ScreenUtil().setHeight(50),
+                          width: ScreenUtil().setWidth(290),
+                          controller: passwordController,
+                          prefixIcon: const SizedBox(),
+                          hintText: 'password'.tr,
+                          isPasswordTextField: true,
+                          keyboardType: TextInputType.text,
+                        ),
                         Container(
                           margin: EdgeInsets.only(right: ScreenUtil().setWidth(10)),
                           alignment: Alignment.topRight,
-                          child: Text('forgotPassword'.tr , style: TextStyle(color: AppColors.fontColor , fontSize: 14.sp , fontWeight: FontWeight.w500),),
+                          child: Text(
+                            'forgotPassword'.tr,
+                            style: TextStyle(
+                              color: AppColors.fontColor,
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
                         ),
-                        SizedBox(height: ScreenUtil().setHeight(20),),
+                        SizedBox(height: ScreenUtil().setHeight(20)),
                         Container(
-                          margin: EdgeInsets.only(right: ScreenUtil().setWidth(10) , left: ScreenUtil().setWidth(10)),
+                          margin: EdgeInsets.symmetric(horizontal: ScreenUtil().setWidth(10)),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              ButtonWidget(width: ScreenUtil().setWidth(40), height: ScreenUtil().setHeight(25), onTap: (){checkEmail(emailController.value.text.toString() , passwordController.value.text.toString());}, name: 'signIn'.tr),
-                              ButtonWidget(width: ScreenUtil().setWidth(40), height: ScreenUtil().setHeight(25), onTap: (){}, name: 'signUp'.tr),
+                              ButtonWidget(
+                                width: ScreenUtil().setWidth(40),
+                                height: ScreenUtil().setHeight(25),
+                                onTap: () {
+                                  checkEmail(
+                                    emailController.value.text.toString(),
+                                    passwordController.value.text.toString(),
+                                  );
+                                },
+                                name: 'signIn'.tr,
+                              ),
+                              ButtonWidget(
+                                width: ScreenUtil().setWidth(40),
+                                height: ScreenUtil().setHeight(25),
+                                onTap: () {},
+                                name: 'signUp'.tr,
+                              ),
                             ],
                           ),
-                        )
+                        ),
                       ],
                     ),
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
@@ -83,7 +130,7 @@ class _LoginViewState extends State<LoginView> implements LoginViewComponent{
   }
 
   @override
-  void checkEmail(String email , password) {
-    component.checkEmailAndPassword(email , password);
+  void checkEmail(String email, password) {
+    component.checkEmailAndPassword(email, password);
   }
 }
